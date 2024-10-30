6beb5d3b7511253fb5833581d6b12d0c34b66e10
Venkat Nallam <93982601+nallamsaiv@users.noreply.github.com>
2024-10-30 13:48:44 -0500

    login page frontend with the background and alignment completed

--- frontend/src/pages/auth/AuthMain.js
+++ frontend/src/pages/auth/AuthMain.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import api from '../../services/api'; // Axios instance with base URL
 import '../../styles/auth.css';
+import logo from '../../assets/beast_mode_logo.png'
 
 function AuthMain() {
   const [username, setUsername] = useState('');
@@ -23,7 +24,10 @@ function AuthMain() {
     <div className="container-fluid d-flex vh-100 align-items-center justify-content-center">
       <div className="row align-items-center justify-content-center w-100">
         <div className="col-md-4 col-sm-8">
+        </div>
+        <div className="col-md-4 offset-md-2 col-sm-8">
           <div className="login-container">
+            <img src={logo} alt="Logo" className="img-fluid mb-3 w-10 h-10" />
             <h1>Welcome</h1>
             <form onSubmit={handleLogin}>
               <input
@@ -47,6 +51,7 @@ function AuthMain() {
         </div>
       </div>
     </div>
+
   );
 }
 
