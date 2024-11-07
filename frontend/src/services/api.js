9ff60397cf9015df90bc31e93530074d0fdf3703
Ethan Nguyen <nguyeeth@tamu.edu>
2024-11-07 02:18:16 -0600

    Adding backup for localhost backend server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,31 @@
 import axios from 'axios';
 
 // our base URL
+const isLocal = window.location.hostname === 'localhost';
 const api = axios.create({
-  baseURL: window.location.hostname === 'localhost'
+  baseURL: isLocal
     ? 'http://127.0.0.1:5001/api/'
     : 'https://project-3-01-beastmode-1fed971de919.herokuapp.com/api/',
+  timeout: isLocal ? 2000 : 10000, 
 });
+
+if (isLocal) {
+  api.interceptors.response.use(
+    response => response, 
+    async error => {
+      // If request fails due to timeout or no response, fall back to remote
+      if (error.config && (error.code === 'ECONNABORTED' || error.response === undefined)) {
+        console.warn('Local API request failed or timed out. Falling back to remote API...');
+        error.config.baseURL = 'https://project-3-01-beastmode-1fed971de919.herokuapp.com/api/';
+        try {
+          return await axios.request(error.config);
+        } catch (err) {
+          return Promise.reject(err);
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+}
 
 export default api;
