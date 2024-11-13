9283d87d5108c092880d3af352864e321cfbca1d
Ethan Nguyen <nguyeeth@tamu.edu>
2024-11-13 08:57:07 -0600

    Actually updating population scripts

--- scripts/table_scripts/populate_employee_info.sql
+++ scripts/table_scripts/populate_employee_info.sql
@@ -1,8 +1,8 @@
 insert into employee_info (employee_id, email, password, first_name, last_name, role)
 values 
-(121202, 'nguyeeth@tamu.edu', 'Beastmode1', 'Ethan', 'Nguyen', 'manager'),
-(312879, 'bgarzonie@tamu.edu', 'Beastmode2', 'Bella', 'Garzonie', 'cashier'),
-(493268, 'nallamsaiv@tamu.edu', 'Beastmode3', 'Venkat', 'Nallam', 'cashier'),
-(523506, 'elliothan@tamu.edu', 'Beastmode4', 'Elliot', 'Han', 'manager'),
-(668453, 'gretta@tamu.edu', 'Beastmode5', 'Gretta', 'Weich', 'manager'),
-(826497, 'ajitrskul@tamu.edu', 'Beastmode6', 'Aurora', 'Jitrskul', 'cashier');
+(121202, 'nguyeeth@tamu.edu', '$2b$10$ae/ZbwKP0ud77Lls7sjfHevzsR6JJ3CalmboJtaT3KS4nuvLWLzii', 'Ethan', 'Nguyen', 'manager'),
+(312879, 'bgarzonie@tamu.edu', '$2b$10$ypqcNuhlRv6A2H8e4QBXA.BRzI0Wzs4ksTYWOqPZi/.73WJo1bTiC', 'Bella', 'Garzonie', 'cashier'),
+(493268, 'nallamsaiv@tamu.edu', '$2b$10$L8Y8KBW73.xGO5MYg6VvouylIcWX/YP20xwPvvYTfEXPnvM8Y6AbC', 'Venkat', 'Nallam', 'cashier'),
+(523506, 'elliothan@tamu.edu', '$2b$10$sFqgUyARsgKymPATw6DBBumnaIsBrfpjEpDei.zEum3mANeeCg5EG', 'Elliot', 'Han', 'manager'),
+(668453, 'gretta@tamu.edu', '$2b$10$QiOzIFf05L/DrHnukYwGNeji2KEwdMOjPKFwOErMwzPNM3wq7o4uy', 'Gretta', 'Weich', 'manager'),
+(826497, 'ajitrskul@tamu.edu', '$2b$10$ypqcNuhlRv6A2H8e4QBXA.BRzI0Wzs4ksTYWOqPZi/.73WJo1bTiC', 'Aurora', 'Jitrskul', 'cashier');
