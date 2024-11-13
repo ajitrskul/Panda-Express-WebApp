82bd3986279f640264aaca1e9d27561c106573e0
Venkat Nallam <93982601+nallamsaiv@users.noreply.github.com>
2024-11-13 15:58:39 -0600

    fixed google oauth

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -10,6 +10,8 @@ from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
 from flask import current_app
+import base64
+import json
 
 GOOGLE_CLIENT_ID = "691942944903-g8cmnfe0iu3jujav9jpgonda6dkj9b8u.apps.googleusercontent.com"
 
@@ -76,42 +78,48 @@ def authenticate_google():
 
 @auth_bp.route("/callback")
 def callback():
-    try:
-        flow = current_app.config['OAUTH_FLOW']
-        flow.fetch_token(authorization_response=request.url)
-        
-        if not session.get("state") == request.args.get("state"):
-            abort(500)
+    flow = current_app.config['OAUTH_FLOW']
+    flow.fetch_token(authorization_response=request.url)
+    
+    if not session.get("state") == request.args.get("state"):
+        abort(500)
 
-        credentials = flow.credentials
-        request_session = requests.session()
-        cached_session = cachecontrol.CacheControl(request_session)
-        token_request = google.auth.transport.requests.Request(session=cached_session)
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
 
-        id_info = id_token.verify_oauth2_token(
-            id_token=credentials._id_token,
-            request=token_request,
-            audience=GOOGLE_CLIENT_ID,
-            clock_skew_in_seconds=60
-        )
+    # id_info = id_token.verify_oauth2_token(
+    #     id_token=credentials._id_token,
+    #     request=token_request,
+    #     audience=GOOGLE_CLIENT_ID,
+    #     clock_skew_in_seconds=60
+    # )
+    
+    id_token_str = credentials._id_token
+    token_parts = id_token_str.split('.')
 
-        session["name"] = id_info.get("name")
-        session["email"] = id_info.get("email")
-        email = id_info.get("email")
+    payload_encoded = token_parts[1]
+
+    padding = '=' * (4 - len(payload_encoded) % 4)
+    payload_decoded = base64.urlsafe_b64decode(payload_encoded + padding)
+    
+    payload_json = json.loads(payload_decoded)
+
+    session["name"] = payload_json.get("name")
+    session["email"] = payload_json.get("email")
+    email = payload_json.get("email")
 
 
-        with db.session.begin():
-            employee = db.session.query(Employee).filter_by(email=email).first()
+    with db.session.begin():
+        employee = db.session.query(Employee).filter_by(email=email).first()
 
-        if employee:
-            if employee.role == 'manager':
-                re_route_link = current_app.config['base_url'] + "/manager"
-            else:
-                re_route_link = current_app.config['base_url'] + "/pos"
-            return redirect(re_route_link)
+    if employee:
+        if employee.role == 'manager':
+            re_route_link = current_app.config['base_url'] + "/manager"
         else:
-            re_route_link = current_app.config['base_url'] + "/auth/signin/error"
-            return redirect(re_route_link)
-    except:
-        re_route_link = current_app.config['base_url'] + "/auth/signup/error"
+            re_route_link = current_app.config['base_url'] + "/pos"
+        return redirect(re_route_link)
+    else:
+        re_route_link = current_app.config['base_url'] + "/auth/signin/error"
         return redirect(re_route_link)
