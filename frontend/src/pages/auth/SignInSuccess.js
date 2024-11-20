4405dd5787ae92bd93ae325628a5a1ddbfe82e04
Aurora Jitrskul <aurawra@outlook.com>
2024-11-20 16:02:21 -0600

    Added redirect on customer signin

--- frontend/src/pages/auth/SignInSuccess.js
+++ frontend/src/pages/auth/SignInSuccess.js
@@ -1,11 +1,29 @@
-import { useContext } from 'react';
-import { Route, Routes, Link } from "react-router-dom";
+import { useContext, useState, useEffect } from 'react';
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
 import { NavBar } from "../kiosk/components/NavBar";
 import "../../styles/signin/SignInSuccess.css";
 import { AccountContext } from './components/AccountContext';
 
 export default function SignInSuccess() {
   const { customer } = useContext(AccountContext);
+  const [timer, setTimer] = useState(3);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Set up an interval to increment the counter every second
+    const interval = setInterval(() => {
+      setTimer(timer-1);
+      if (timer === 1) {
+        navigate('/kiosk');
+        window.location.reload();
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <Routes>
       <Route
@@ -22,11 +40,12 @@ export default function SignInSuccess() {
               </div>
               <div className="row justify-content-center">
                 <div className="col-6 text-center">
-                  <h1 className="signin-success-title">Signed In Successfully!</h1>
+                  <h1 className="signin-success-title">Signed In </h1>
+                  <h1 className="signin-success-title" style={{marginTop:"-30px"}}>Successfully!</h1>
                   <p className="signin-success-links">
                     Welcome <span style={{fontWeight:"bold"}}>{customer.first_name} {customer.last_name}</span>
                   </p>
-                  <Link className="signin-success-links" to="/kiosk" tabIndex="1">Return to Kiosk</Link>
+                  <Link className="signin-success-links" to="/kiosk" tabIndex="1">Returning to Kiosk...{timer}</Link>
                 </div>
               </div>
             </div>
